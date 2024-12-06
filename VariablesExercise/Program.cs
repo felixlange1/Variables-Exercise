0aa79abc22c67f879c0291dde62e2b7121b63162
Felix Lange <felixlange1@gmail.com>
2024-12-05 23:34:37 -0800

    variables declared and interpolated

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+            string firstName = "Fernando";
+            int age = 27;
+            char middleInitial = 'M';
+            bool isMale = true;
+            double heightinMeters = 1.81;
+            decimal moneyAvailable = 1000.59m;
+
+            Console.WriteLine(
+                $"Meet {firstName} {middleInitial}. He's {age} years old and {heightinMeters} meters tall. He is worth {moneyAvailable}."
+                );
         }
     }
 }
